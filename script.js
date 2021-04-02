3997be171a6d4b00de6d3aab7ac95ec37f507774
Alina Mironova <tommilin-07.13@yandex.ru>
2021-04-02 18:08:20 +0300

    Fetch

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert('Hello World')
+let url = window.location.search;
+function getName(url) {
+	let getUrl = url.split('=');
+	let name = getUrl[1];
+	if(name == null) {
+	name = 'MironovaAlina';
+	}
+	return name;
+}
+function getInfo() {
+  fetch(`https://api.github.com/users/${getName(url)}`)
+  .then(res => res.json())
+  .then(json => {
+    let avatar = document.createElement('img');
+    if(json.avatar_url != null) {
+      avatar.src = json.avatar_url;
+    } else {
+      document.body.innerHTML= ('Аватар отсутствует<br>');
+    }
+    let link = document.createElement('a');
+    link.href = json.html_url;
+    if(json.login != null) {
+      link.innerHTML = json.login +'<br>';
+    } else {
+      link.innerHTML = 'Нет информации об имени<br>';
+    }
+    if(json.bio != null) {
+      document.body.innerHTML= json.bio +'<br>';
+    } else {
+      document.body.innerHTML= ('Нет информации о биографии<br>');
+    }
+    document.body.append(link);
+    document.body.append(avatar);
+    })
+  .catch(err => document.body.innerHTML = ('Информация о пользователе недоступна'));
+}
+getInfo(name);
+console.log(window.location.search);
